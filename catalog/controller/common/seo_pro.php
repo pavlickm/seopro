09d7bae2fdc94f75e947c9bf17493f3e39210426
Shvarev Ruslan <shvarev.ruslan@otr.ru>
2013-05-27 12:58:07 +0400

    fix 1.5.4+ manufacturer seo page

--- catalog/controller/common/seo_pro.php
+++ catalog/controller/common/seo_pro.php
@@ -120,7 +120,12 @@ class ControllerCommonSeoPro extends Controller {
 			} elseif (isset($this->request->get['path'])) {
 				$this->request->get['route'] = 'product/category';
 			} elseif (isset($this->request->get['manufacturer_id'])) {
-				$this->request->get['route'] = 'product/manufacturer/product';
+
+                $this->request->get['route'] = 'product/manufacturer/product';
+
+                if(in_array(substr(VERSION, 0, 5), array('1.5.4', '1.5.5'))) { 
+				    $this->request->get['route'] = 'product/manufacturer/info';
+                }
 			} elseif (isset($this->request->get['information_id'])) {
 				$this->request->get['route'] = 'information/information';
 			} else {
